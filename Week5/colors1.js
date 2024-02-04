228242532e2679c85c8874ede202174df0aedec0
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-02-04 17:06:58 +0530

    Add files via upload

---
+++ Week5/colors1.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = function() {
+            document.querySelector('#hello').style.color = button.dataset.color;
+        }
+    });
+
+});
